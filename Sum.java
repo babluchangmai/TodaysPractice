f7bb0bd34a83ed8b029ca5ac225ee45188da65a7
babluchangmai <changmaisiv@gmail.com>
2026-07-03 12:23:10 +0530

    Adding a java program two add two nos:

---
+++ Sum.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class Sum{
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter two integers to calculate their sum:");
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int sum = a + b;
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+    }
+}
